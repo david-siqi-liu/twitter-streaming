e88b381cf600f687195774d9ca86d552ea71ca90
David Liu <davidsiqiliu@gmail.com>
2019-11-17 14:25:08 -0500

    Updated

--- src/main/java/twitterstreaming/TwitterStream.java
+++ src/main/java/twitterstreaming/TwitterStream.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package twitterstreaming;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
@@ -24,7 +6,6 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
-import org.apache.flink.streaming.examples.twitter.util.TwitterExampleData;
 import org.apache.flink.util.Collector;
 
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
@@ -32,37 +13,109 @@ import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMap
 
 import java.util.StringTokenizer;
 
+import twitterstreaming.util.TwitterExampleData;
+
 /**
  * Implements the "TwitterStream" program that computes a most used word
  * occurrence over JSON objects in a streaming fashion.
  */
 public class TwitterStream {
 
-	public static void main(String[] args) throws Exception {
-		// set up the streaming execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+	// *************************************************************************
+	// PROGRAM
+	// *************************************************************************
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataStream<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * http://flink.apache.org/docs/latest/apis/streaming/index.html
-		 *
+    public static void main(String[] args) throws Exception {
+
+		// Checking input parameters
+		final ParameterTool params = ParameterTool.fromArgs(args);
+		System.out.println("Usage: TwitterExample " +
+				"[--output <path>]" +
+				"[--twitter-source.consumerKey <key>]" +
+				"[--twitter-source.consumerSecret <secret>]" +
+				"[--twitter-source.token <token>]" +
+				"[--twitter-source.tokenSecret <tokenSecret>]");
+
+		// Set up the streaming execution environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		// Get input data
+		DataStream<String> streamSource;
+		if (params.has(TwitterSource.CONSUMER_KEY) &&
+				params.has(TwitterSource.CONSUMER_SECRET) &&
+				params.has(TwitterSource.TOKEN) &&
+				params.has(TwitterSource.TOKEN_SECRET)
+		) {
+			streamSource = env.addSource(new TwitterSource(params.getProperties()));
+		} else {
+			System.out.println("Executing TwitterStream example with default props.");
+			System.out.println("Use --twitter-source.consumerKey <key> --twitter-source.consumerSecret <secret> " +
+					"--twitter-source.token <token> --twitter-source.tokenSecret <tokenSecret> specify the authentication info.");
+			// get default test text data
+			streamSource = env.fromElements(TwitterExampleData.TEXTS);
+		}
+
+        // Get tweets
+		DataStream<Tuple2<String, Integer>> tweets = streamSource
+				// Selecting English tweets and splitting to (word, 1)
+				.flatMap(new SelectEnglishAndTokenizeFlatMap())
+				// Group by words and sum their occurrences
+				.keyBy(0)
+				.sum(1);
+
+		// Emit result
+		if (params.has("output")) {
+			tweets.writeAsText(params.get("output"));
+		} else {
+			System.out.println("Printing result to stdout. Use --output to specify output path.");
+			tweets.print();
+		}
+
+		// Execute program
+		env.execute("Twitter Stream");
+    }
+
+	// *************************************************************************
+	// USER FUNCTIONS
+	// *************************************************************************
+
+	/**
+	 * Deserialize JSON from twitter source
+	 *
+	 * <p>Implements a string tokenizer that splits sentences into words as a
+	 * user-defined FlatMapFunction. The function takes a line (String) and
+	 * splits it into multiple pairs in the form of "(word,1)" ({@code Tuple2<String,
+	 * Integer>}).
+	 */
+	public static class SelectEnglishAndTokenizeFlatMap implements FlatMapFunction<String, Tuple2<String, Integer>> {
+		private static final long serialVersionUID = 1L;
+
+		private transient ObjectMapper jsonParser;
+
+		/**
+		 * Select the language from the incoming JSON text.
 		 */
+		@Override
+		public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+			if (jsonParser == null) {
+				jsonParser = new ObjectMapper();
+			}
+			JsonNode jsonNode = jsonParser.readValue(value, JsonNode.class);
+			boolean isEnglish = jsonNode.has("user") && jsonNode.get("user").has("lang") && jsonNode.get("user").get("lang").asText().equals("en");
+			boolean hasText = jsonNode.has("text");
+			if (isEnglish && hasText) {
+				// message of tweet
+				StringTokenizer tokenizer = new StringTokenizer(jsonNode.get("text").asText());
 
-		// execute program
-		env.execute("Flink Streaming Java API Skeleton");
+				// split the message
+				while (tokenizer.hasMoreTokens()) {
+					String result = tokenizer.nextToken().replaceAll("\\s*", "").toLowerCase();
+
+					if (!result.equals("")) {
+						out.collect(new Tuple2<>(result, 1));
+					}
+				}
+			}
+		}
 	}
 }
