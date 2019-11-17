8460a794274200b24317a64e2c9bf6a7a0973b4a
David Liu <davidsiqiliu@gmail.com>
2019-11-17 12:10:15 -0500

    Initial commit

---
+++ src/main/java/twitterstreaming/TwitterSource.java
@@ -0,0 +1,11 @@
+package twitterstreaming;
+
+/**
+ * Input Twitter API credentials here
+ */
+public class TwitterSource {
+    public static final String CONSUMER_KEY = "consumerKey";
+    public static final String CONSUMER_SECRET = "consumerSecret";
+    public static final String TOKEN = "accessToken";
+    public static final String TOKEN_SECRET = "accessTokenSecret";
+}
