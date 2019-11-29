01eac6b2c43ec1e18cdeb16bfb53eb6b469bc08c
David Liu <davidsiqiliu@gmail.com>
2019-11-29 01:29:28 -0500

    Optimized imports

--- src/main/java/twitterstreaming/elasticsearch/Client.java
+++ src/main/java/twitterstreaming/elasticsearch/Client.java
@@ -1,27 +1,11 @@
 package twitterstreaming.elasticsearch;
 
-import org.apache.commons.compress.compressors.zstandard.ZstdCompressorOutputStream;
 import org.apache.http.HttpHost;
-import org.elasticsearch.ElasticsearchException;
-import org.elasticsearch.action.DocWriteResponse;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
-import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.support.master.AcknowledgedResponse;
-import org.elasticsearch.client.*;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.Requests;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.rest.RestStatus;
-import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 
 /**
